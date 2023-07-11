d64c3a3fcc95ac4719f4c130f7c0fb0a7809df4a
Tim Jenness <tjenness@lsst.org>
2023-07-11 15:36:03 -0700

    Revert "disable tests"
    This reverts commit 3cc715ce43acc5eda6b0115a74d7a4e2c917774a.
    Parsl is now included in the environment.

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,15 +1,12 @@
-import pytest
+from lsst.ctrl.bps import BpsConfig
+from lsst.ctrl.bps.parsl.configuration import get_bps_config_value
 from lsst.daf.butler import Config
 
 
-@pytest.mark.skip(reason="parsl not in LSST environment")
 def test_config():
     """Super-basic test of configuration reading
 
     This is intended as a test of testing more than anything else.
     """
-    from lsst.ctrl.bps import BpsConfig
-    from lsst.ctrl.bps.parsl.configuration import get_bps_config_value
-
     config = BpsConfig(Config.fromString("foo: bar"))  # BpsConfig doesn't work directly with fromString
     assert get_bps_config_value(config, "foo", str) == "bar"
--- tests/test_import.py
+++ tests/test_import.py
@@ -1,6 +1,2 @@
-import pytest
-
-
-@pytest.mark.skip(reason="parsl not in LSST environment")
 def test_import():
     import lsst.ctrl.bps.parsl  # noqa
